d0f17b80d1a8f894db88cdbe6867d48f2fbb21a7
wallaceeny <bongobonga07@gmail.com>
2024-07-27 18:39:39 +0200

    j

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-document.getElementById('show-register').addEventListener('click', function() {
-    document.getElementById('login-form-container').style.display = 'none';
-    document.getElementById('register-form-container').style.display = 'block';
-  });
-  
-  document.getElementById('show-login').addEventListener('click', function() {
-    document.getElementById('login-form-container').style.display = 'block';
-    document.getElementById('register-form-container').style.display = 'none';
-  });
-  
-  document.getElementById('login-form').addEventListener('submit', function(event) {
+document.getElementById('login-form')?.addEventListener('submit', function(event) {
     event.preventDefault();
     const username = document.getElementById('login-username').value;
-    // Simulating successful login by storing the username
     localStorage.setItem('loggedIn', username);
+    localStorage.setItem('points', '0');
+    localStorage.setItem('level', '0');
     window.location.href = 'home.html';
   });
   
-  document.getElementById('register-form').addEventListener('submit', function(event) {
+  document.getElementById('register-form')?.addEventListener('submit', function(event) {
     event.preventDefault();
     const username = document.getElementById('register-username').value;
-    // Simulating successful registration by storing the username
     localStorage.setItem('loggedIn', username);
-    window.location.href = 'home.html';
+    localStorage.setItem('points', '0');
+    localStorage.setItem('level', '0');
+    window.location.href = 'login.html';
   });
   
